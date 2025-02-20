97c3c373b4c681ab6bc6f842fca6c8dfe0344b76
Alejandra <alejandrasandoval4321@gmail.com>
2025-02-20 17:43:53 -0600

    modelado de tabla usuarios

--- L01_NUMEROS_CARNETS/Models/usuarios.cs
+++ L01_NUMEROS_CARNETS/Models/usuarios.cs
@@ -1,6 +1,14 @@
-﻿namespace L01_NUMEROS_CARNETS.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace L01_NUMEROS_CARNETS.Models
 {
     public class usuarios
     {
+        [Key]
+        public int id_usuario { get; set; }
+        public int id_rol { get; set; }
+        public string nombreUsuario { get; set; }
+        public string clave { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
     }
 }
