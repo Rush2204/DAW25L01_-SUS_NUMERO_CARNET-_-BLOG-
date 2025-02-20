c61abf6f45341f8ecb0acabb56898f306b93d302
Alejandra <alejandrasandoval4321@gmail.com>
2025-02-20 17:28:40 -0600

    Inyeccion de dependencias

--- L01_NUMEROS_CARNETS/Program.cs
+++ L01_NUMEROS_CARNETS/Program.cs
@@ -1,8 +1,13 @@
+using L01_NUMEROS_CARNETS.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<BlogDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("blogDBConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
