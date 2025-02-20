48ba9aad374f6e4200d5a85f68df1746e5b39b8e
Alejandra <alejandrasandoval4321@gmail.com>
2025-02-20 17:43:28 -0600

    modelado de tabla roles

--- L01_NUMEROS_CARNETS/Models/roles.cs
+++ L01_NUMEROS_CARNETS/Models/roles.cs
@@ -1,6 +1,10 @@
-﻿namespace L01_NUMEROS_CARNETS.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace L01_NUMEROS_CARNETS.Models
 {
     public class roles
     {
+        [Key]
+        public int id_rol { get; set; }
+        public string rol { get; set; }
     }
 }
